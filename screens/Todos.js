373930f69911ebb008b2483cf26e0713857180c0
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-06-01 13:20:45 -0700

    calendar show

--- screens/Todos.js
+++ screens/Todos.js
@@ -1,10 +1,38 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import {
+  CalendarList,
+  Calendar,
+  Agenda,
+  LocaleConfig,
+} from "react-native-calendars";
 
 export default function Todos() {
+  const [selected, setSelected] = useState("");
+
+  const currentDate = new Date();
+
   return (
-    <View>
-      <Text>Todos</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <Calendar
+        onDayPress={(day) => {
+          setSelected(day.dateString);
+        }}
+        style={{ height: 500, width: 500, borderWidth: 1, borderColor: "gray" }}
+        current={currentDate}
+        markedDates={{
+          [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+            selectedDotColor: "orange",
+          },
+        }}
+      />
     </View>
   );
 }
